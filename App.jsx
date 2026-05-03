699a3abb711f920ad46f5f78e6c2004c30a58a85
MUKESH YADAV <mukeshy32109@gmail.com>
2026-05-03 16:23:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Weather from './Components/Weather'
+import Animation from './Components/Animation'
+import Signature from './Components/Signature'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Animation/> 
+      <Weather/>
+      <Signature/>
+        
+    </div>
+    
+  )
+}
+
+export default App
